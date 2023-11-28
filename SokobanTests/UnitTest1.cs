97a78c0f006753d110400556268374ffb44b7fbc
Iván Moreno <imoreno.owo@gmail.com>
2023-11-28 16:43:45 +0100

    docs: define kata goals

--- SokobanTests/UnitTest1.cs
+++ SokobanTests/UnitTest1.cs
@@ -1,12 +1,13 @@
 namespace SokobanTests;
 
+// 1. El personaje se puede mover en las cuatro direcciones (arriba, abajo, izquierda, derecha)
+// 2. El personaje no puede moverse a través de una pared
+// 3. El personaje puede empujar una caja hacia la dirección en la que se mueve
+// 4. El personaje no puede empujar una caja si hay una pared o una caja bloqueando el camino
+// 5. Todas las cajas deben estar sobre un objetivo para ganar el juego
+
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
