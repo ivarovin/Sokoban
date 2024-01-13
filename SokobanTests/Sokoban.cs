47bed3939027cef9dd8dc8ac5d8366251f866f99
knexator <daniel.hurtadoramirez314@gmail.com>
2024-01-13 16:18:07 +0100

    feat: new Position struct

--- SokobanTests/Sokoban.cs
+++ SokobanTests/Sokoban.cs
@@ -2,6 +2,17 @@ using FluentAssertions;
 
 namespace SokobanTests;
 
+public struct Position {
+    public readonly int x, y;    
+
+    public Position(int x, int y)  {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static implicit operator (int, int) (Position position) => (position.x, position.y);
+}
+
 public class Sokoban
 {
     public readonly (int, int)[] Targets;
