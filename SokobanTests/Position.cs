e0556c7d3adf1272f63c7c8bd99c1ba0c1996585
unknown <daniel.hurtadoramirez314@gmail.com>
2024-01-22 18:20:56 +0100

    feat: add Direction class

--- SokobanTests/Position.cs
+++ SokobanTests/Position.cs
@@ -13,3 +13,17 @@ public struct Position
     public static implicit operator (int, int)(Position position) => (position.x, position.y);
     public static implicit operator Position((int, int) position) => new(position.Item1, position.Item2);
 }
+
+public struct Direction
+{
+    public readonly int dx, dy;
+
+    public Direction(int dx, int dy)
+    {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public static implicit operator (int, int)(Direction direction) => (direction.dx, direction.dy);
+    public static implicit operator Direction((int, int) direction) => new(direction.Item1, direction.Item2);
+}
